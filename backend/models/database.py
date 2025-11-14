50d3364352d0c5667c2b850b55f075455abc1477
Diviyan Rajan <diviyanr@gmail.com>
2025-11-14 11:13:38 +0800

    Change database from MongoDB to Supabase

--- backend/models/database.py
+++ backend/models/database.py
@@ -1,11 +1,21 @@
-import os
-from pymongo import MongoClient
+import os, requests
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-def connect_to_database():
-    connection_string = os.getenv("MONGODB_CONNECTION_STRING")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+API_KEY = os.getenv("SUPABASE_API_KEY")
+BEARER_TOKEN = os.getenv("SUPABASE_BEARER_TOKEN")
 
-    client = MongoClient(connection_string)
-    return client
+url = f"{SUPABASE_URL}/rest/v1/heartbeats?select=*"
+
+headers = {
+    "apikey": API_KEY,
+    "Authorization": f"Bearer {BEARER_TOKEN}",
+    "Content-Type": "application/json"
+}
+
+response = requests.get(url, headers=headers)
+
+print("Status:", response.status_code)
+print("Data:", response.json())
